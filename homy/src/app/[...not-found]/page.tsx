9e1ba87544dd00484fcc0a047f5d3d3daba0bad7
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-20 20:04:58 +0300

    changes dynamically rendered [age to a static page

--- homy/src/app/[...not-found]/page.tsx
+++ homy/src/app/[...not-found]/page.tsx
@@ -1,15 +1,19 @@
+export async function generateStaticParams() {
+  return [{ notFound: ["404"] }];
+}
+
 import Error from "@/components/inner-pages/error";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-   title: "404 error || Homy - Real Estate React Next js Template",
+  title: "404 error || Homy - Real Estate React Next js Template",
 };
 const index = () => {
-   return (
-      <Wrapper>
-         <Error />
-      </Wrapper>
-   )
-}
+  return (
+    <Wrapper>
+      <Error />
+    </Wrapper>
+  );
+};
 
-export default index
+export default index;
