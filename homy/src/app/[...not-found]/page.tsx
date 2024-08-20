12085265919d12fe76cbdfe3e8b7abcf776ed067
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-20 20:09:08 +0300

    fixes genereatestaticparam function

--- homy/src/app/[...not-found]/page.tsx
+++ homy/src/app/[...not-found]/page.tsx
@@ -1,13 +1,13 @@
-export async function generateStaticParams() {
-  return [{ notFound: ["404"] }];
-}
-
 import Error from "@/components/inner-pages/error";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
   title: "404 error || Homy - Real Estate React Next js Template",
 };
+
+export async function generateStaticParams() {
+  return [{ "not-found": [] }];
+}
 const index = () => {
   return (
     <Wrapper>
