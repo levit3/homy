a8719a29ea709db67a8b9098bab7f6f3e490565d
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-20 20:37:11 +0300

    adds aliases to make sure images are read in deployment

--- homy/next.config.js
+++ homy/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+const nextConfig = {
+  output: "export",
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.alias["@/assets"] = path.resolve(__dirname, "public/assets");
+    return config;
+  },
+};
 
 module.exports = nextConfig;
