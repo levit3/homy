e74e780c628e6a9bc70a5d63e367568b95536804
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-20 21:13:14 +0300

    makes few changes

--- homy/src/app/404.tsx
+++ homy/src/app/404.tsx
@@ -5,9 +5,6 @@ export const metadata = {
   title: "404 error || Homy - Real Estate React Next js Template",
 };
 
-export async function generateStaticParams() {
-  return [{ "not-found": [] }];
-}
 const index = () => {
   return (
     <Wrapper>
