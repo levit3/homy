392ee2f7117dfead137e7eadc2b851246d3a93c8
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-08-20 19:30:16 +0300

    removes previous builder.io configs

--- homy/next.config.js
+++ homy/next.config.js
@@ -1,6 +1,4 @@
-const withBuilderDevTools = require("@builder.io/dev-tools/next")();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({});
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
